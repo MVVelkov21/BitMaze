51bb970237a7c2ab628d54154dd08200c1ea92f1
MVVelkov21 <MVVelkov21@codingburgas.bg>
2023-03-12 16:17:52 +0200

    Add small changes

--- Game/BitMaze/BitMaze/MainMenu.cpp
+++ Game/BitMaze/BitMaze/MainMenu.cpp
@@ -9,6 +9,7 @@ const int playSquareW = 150;
 const int playX = 335;
 const int playY = 128;
 const int playS = 50;
+const int playKey = KEY_ONE;
 
 const int rulesSquareX = 308;
 const int rulesSquareY = 185;
@@ -16,14 +17,22 @@ const int rulesSquareW = 185;
 const int rulesX = 318;
 const int rulesY = 188;
 const int rulesS = 50;
+const int rulesKey = KEY_TWO;
 
-void MainMenuButtons(const int squareX, const int squareY, const int squareW, const int W, const int H, const char Text[]) {
+void MainMenuButtons(const int squareX, const int squareY, const int squareW, const int W, const int H, const char Text[], const int key) {
     DrawRectangle(squareX, squareY, squareW, 50, BLACK);
     DrawText(Text, W, H, 50, WHITE);
+    if (IsKeyPressed(key)) {
+        while (!WindowShouldClose()) {
+            BeginDrawing();
+            DrawText(Text, W, H, 50, RED);
+            EndDrawing();
+        }
+    }
 }
 
 void MainMenuGUI() {
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - sprite button");
+    InitWindow(screenWidth, screenHeight, "BitMaze");
 
     DisableCursor();
 
@@ -34,17 +43,8 @@ void MainMenuGUI() {
         BeginDrawing();
         ClearBackground(WHITE);
         DrawText("PRESS 'ENTER' TO PLAY, 'R' FOR RULES OR 'ESC' TO QUIT", 100, 100, 20, BLACK);
-        MainMenuButtons(playSquareX, playSquareY, playSquareW, playX, playY, "PLAY");
-        MainMenuButtons(rulesSquareX, rulesSquareY, rulesSquareW, rulesX, rulesY, "RULES");
-        if (IsKeyPressed(KEY_ENTER)) {
-            while (!WindowShouldClose()) {
-                BeginDrawing();
-                DrawText("PLAY", playX, playY, playS, RED);
-                EndDrawing();
-            }
-        }
-
+        MainMenuButtons(playSquareX, playSquareY, playSquareW, playX, playY, "PLAY", playKey);
+        MainMenuButtons(rulesSquareX, rulesSquareY, rulesSquareW, rulesX, rulesY, "RULES", rulesKey);
         EndDrawing();
     }
-    CloseWindow();
 }
