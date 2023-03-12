153163a06c5368e9a774f63c7817e20dab8ab197
MVVelkov21 <MVVelkov21@codingburgas.bg>
2023-03-12 14:42:20 +0200

    Add basic button

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -1,15 +1,33 @@
 #include "raylib.h"
 
-int main(){
+int main()
+{
     const int screenWidth = 800;
     const int screenHeight = 450;
+    const int playW = 335;
+    const int playH = 128;
+    const int playS = 50;
+    bool play = 0;
 
-    InitWindow(screenWidth, screenHeight, "Test");
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - sprite button");
+
+    DisableCursor();
+
     SetTargetFPS(60);
-    while(!WindowShouldClose()) {
+    
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Test", 360, 200, 20, BLACK);
+            ClearBackground(WHITE);
+            DrawRectangle(325, 125, 150, 50, BLACK);
+            DrawText("PLAY", playW, playH, playS, WHITE);
+            if (IsKeyPressed(KEY_ENTER)) {
+                while (!WindowShouldClose()) {
+                    BeginDrawing();
+                    DrawText("PLAY", playW, playH, playS, RED);
+                    EndDrawing();
+                }
+            }
         EndDrawing();
     }
     CloseWindow();
