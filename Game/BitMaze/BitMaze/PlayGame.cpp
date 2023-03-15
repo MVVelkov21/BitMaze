5137290151549e09ad50256c1f25c7adcd73413f
MVVelkov21 <MVVelkov21@codingburgas.bg>
2023-03-15 19:43:53 +0200

    Add game menu gui

--- Game/BitMaze/BitMaze/PlayGame.cpp
+++ Game/BitMaze/BitMaze/PlayGame.cpp
@@ -1,6 +1,63 @@
 #include "raylib.h"
 #include "MainMenu.h"
 
+void levelButtons() {
+	if (IsKeyPressed(KEY_ONE)) {
+		SetExitKey(KEY_ONE);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("LEVEL 1", 110, 205, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+	if (IsKeyPressed(KEY_FOUR)) {
+		SetExitKey(KEY_FOUR);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("LEVEL 4", 110, 380, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+	if (IsKeyPressed(KEY_TWO)) {
+		SetExitKey(KEY_TWO);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("LEVEL 2", 335, 205, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+	if (IsKeyPressed(KEY_FIVE)) {
+		SetExitKey(KEY_FIVE);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("LEVEL 5", 335, 380, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+	if (IsKeyPressed(KEY_THREE)) {
+		SetExitKey(KEY_THREE);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("LEVEL 3", 560, 205, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+	if (IsKeyPressed(KEY_SIX)) {
+		SetExitKey(KEY_SIX);
+		while (!WindowShouldClose()) {
+			BeginDrawing();
+			DrawText("RANDOM", 560, 380, 30, RED);
+			EndDrawing();
+		}
+		SetExitKey(KEY_ESCAPE);
+	}
+}
+
 void GameMainMenu() {
 	while (!WindowShouldClose()){
 		if (IsKeyPressed(KEY_ESCAPE)) {
@@ -16,6 +73,19 @@ void GameMainMenu() {
 		DrawRectangle(15, 15, 80, 25, BLACK);
 		DrawText("Return", 20, 20, 20, WHITE);
 		DrawText("LEVELS", 288, 30, 50, BLACK);
+		DrawRectangle(70, 100, 200, 150, BLACK);
+		DrawText("LEVEL 1", 110, 205, 30, WHITE);
+		DrawRectangle(70, 275, 200, 150, BLACK);
+		DrawText("LEVEL 4", 110, 380, 30, WHITE);
+		DrawRectangle(295, 100, 200, 150, BLACK);
+		DrawText("LEVEL 2", 335, 205, 30, WHITE);
+		DrawRectangle(295, 275, 200, 150, BLACK);
+		DrawText("LEVEL 5", 335, 380, 30, WHITE);
+		DrawRectangle(520, 100, 200, 150, BLACK);
+		DrawText("LEVEL 3", 560, 205, 30, WHITE);
+		DrawRectangle(520, 275, 200, 150, BLACK);
+		DrawText("RANDOM", 560, 380, 30, WHITE);
+		levelButtons();
 		EndDrawing();
 	}
 }
--- Game/BitMaze/BitMaze/PlayGame.h
+++ Game/BitMaze/BitMaze/PlayGame.h
@@ -1,1 +1,3 @@
 void GameMainMenu();
+
+void levelButtons();
