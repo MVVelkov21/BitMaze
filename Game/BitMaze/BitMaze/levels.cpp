8a16d919c7fde9bb69b10f0cf9e8b6287af73851
MVVelkov21 <MVVelkov21@codingburgas.bg>
2023-03-23 18:02:42 +0200

    Add collisions to level 3

--- Game/BitMaze/BitMaze/levels.cpp
+++ Game/BitMaze/BitMaze/levels.cpp
@@ -527,161 +527,201 @@ void level3() {
 	camera.rotation = 0.0f;
 	camera.zoom = 3.0f;
 
-	const int numBoxes = 30;
+	const int numBoxes = 40;
 	Rectangle boxes[numBoxes];
-	//Color boxColor = BLANK; 
-	Color boxColor = RED;
-	boxes[0].x = 180;
+	Color boxColor = BLANK; 
+	//Color boxColor = RED;
+	boxes[0].x = 175;
 	boxes[0].y = 0;
 	boxes[0].width = 7;
 	boxes[0].height = 500;
 
-	boxes[1].x = 180;
-	boxes[1].y = 2;
-	boxes[1].width = 500;
-	boxes[1].height = 7;
+	boxes[1].x = 617;
+	boxes[1].y = 0;
+	boxes[1].width = 7;
+	boxes[1].height = 500;
 
-	boxes[2].x = 240;
-	boxes[2].y = 405;
-	boxes[2].width = 280;
+	boxes[2].x = 175;
+	boxes[2].y = 0;
+	boxes[2].width = 500;
 	boxes[2].height = 7;
 
-	boxes[3].x = 180;
-	boxes[3].y = 445;
-	boxes[3].width = 500;
-	boxes[3].height = 7;
+	boxes[3].x = 216;
+	boxes[3].y = 0;
+	boxes[3].width = 72;
+	boxes[3].height = 77;
 
-	boxes[4].x = 618;
-	boxes[4].y = 0;
-	boxes[4].width = 7;
-	boxes[4].height = 500;
+	boxes[4].x = 210;
+	boxes[4].y = 320;
+	boxes[4].width = 72;
+	boxes[4].height = 77;
 
-	boxes[5].x = 557;
-	boxes[5].y = 64;
-	boxes[5].width = 10;
-	boxes[5].height = 136;
+	boxes[5].x = 280;
+	boxes[5].y = 250;
+	boxes[5].width = 54;
+	boxes[5].height = 155;
 
-	boxes[6].x = 180;
-	boxes[6].y = 153;
-	boxes[6].width = 380;
-	boxes[6].height = 10;
+	boxes[6].x = 365;
+	boxes[6].y = 250;
+	boxes[6].width = 74;
+	boxes[6].height = 153;
 
-	boxes[7].x = 243;
-	boxes[7].y = 364;
-	boxes[7].width = 225;
-	boxes[7].height = 10;
+	boxes[7].x = 400;
+	boxes[7].y = 375;
+	boxes[7].width = 155;
+	boxes[7].height = 28;
 
-	boxes[8].x = 510;
-	boxes[8].y = 353;
-	boxes[8].width = 225;
-	boxes[8].height = 10;
+	boxes[8].x = 520;
+	boxes[8].y = 340;
+	boxes[8].width = 35;
+	boxes[8].height = 50;
 
-	boxes[9].x = 226;
-	boxes[9].y = 0;
-	boxes[9].width = 9;
-	boxes[9].height = 65;
+	boxes[9].x = 175;
+	boxes[9].y = 445;
+	boxes[9].width = 500;
+	boxes[9].height = 7;
 
-	boxes[10].x = 226;
-	boxes[10].y = 105;
-	boxes[10].width = 9;
-	boxes[10].height = 20;
+	boxes[10].x = 175;
+	boxes[10].y = 98;
+	boxes[10].width = 160;
+	boxes[10].height = 10;
 
-	boxes[11].x = 340;
-	boxes[11].y = 0;
-	boxes[11].width = 9;
-	boxes[11].height = 130;
+	boxes[11].x = 220;
+	boxes[11].y = 140;
+	boxes[11].width = 88;
+	boxes[11].height = 10;
 
-	boxes[12].x = 226;
-	boxes[12].y = 119;
-	boxes[12].width = 147;
+	boxes[12].x = 332;
+	boxes[12].y = 136;
+	boxes[12].width = 42;
 	boxes[12].height = 10;
 
-	boxes[13].x = 350;
-	boxes[13].y = 50;
-	boxes[13].width = 63;
+	boxes[13].x = 416;
+	boxes[13].y = 138;
+	boxes[13].width = 42;
 	boxes[13].height = 10;
 
-	boxes[14].x = 405;
+	boxes[14].x = 332;
 	boxes[14].y = 45;
-	boxes[14].width = 7;
-	boxes[14].height = 15;
+	boxes[14].width = 42;
+	boxes[14].height = 10;
 
-	boxes[15].x = 418;
-	boxes[15].y = 118;
-	boxes[15].width = 140;
+	boxes[15].x = 420;
+	boxes[15].y = 44;
+	boxes[15].width = 90;
 	boxes[15].height = 10;
 
-	boxes[16].x = 452;
-	boxes[16].y = 63;
-	boxes[16].width = 10;
-	boxes[16].height = 60;
+	boxes[16].x = 300;
+	boxes[16].y = 178;
+	boxes[16].width = 175;
+	boxes[16].height = 10;
 
-	boxes[17].x = 452;
-	boxes[17].y = 63;
-	boxes[17].width = 28;
+	boxes[19].x = 256;
+	boxes[19].y = 220;
+	boxes[19].width = 144;
+	boxes[19].height = 10;
+
+	boxes[17].x = 216;
+	boxes[17].y = 286;
+	boxes[17].width = 46;
 	boxes[17].height = 10;
 
-	boxes[18].x = 530;
-	boxes[18].y = 64;
-	boxes[18].width = 28;
+	boxes[18].x = 175;
+	boxes[18].y = 396;
+	boxes[18].width = 46;
 	boxes[18].height = 10;
 
-	boxes[19].x = 242;
-	boxes[19].y = 238;
-	boxes[19].width = 10;
-	boxes[19].height = 135;
+	boxes[20].x = 428;
+	boxes[20].y = 213;
+	boxes[20].width = 25;
+	boxes[20].height = 7;
 
-	boxes[20].x = 302;
-	boxes[20].y = 158;
-	boxes[20].width = 10;
-	boxes[20].height = 95;
+	boxes[21].x = 455;
+	boxes[21].y = 338;
+	boxes[21].width = 75;
+	boxes[21].height = 5;
 
-	boxes[21].x = 302;
-	boxes[21].y = 228;
-	boxes[21].width = 180;
-	boxes[21].height = 7;
-
-	boxes[22].x = 479;
-	boxes[22].y = 228;
+	boxes[22].x = 330;
+	boxes[22].y = 46;
 	boxes[22].width = 10;
-	boxes[22].height = 23;
+	boxes[22].height = 100;
 
-	boxes[23].x = 252;
-	boxes[23].y = 236;
-	boxes[23].width = 50;
-	boxes[23].height = 10;
+	boxes[23].x = 364;
+	boxes[23].y = 100;
+	boxes[23].width = 10;
+	boxes[23].height = 40;
 
-	boxes[24].x = 560;
-	boxes[24].y = 270;
+	boxes[24].x = 416;
+	boxes[24].y = 100;
 	boxes[24].width = 10;
-	boxes[24].height = 43;
+	boxes[24].height = 40;
 
-	boxes[25].x = 304;
-	boxes[25].y = 296;
+	boxes[25].x = 300;
+	boxes[25].y = 140;
 	boxes[25].width = 10;
-	boxes[25].height = 19;
+	boxes[25].height = 40;
 
-	boxes[26].x = 304;
-	boxes[26].y = 305;
-	boxes[26].width = 260;
-	boxes[26].height = 10;
+	boxes[26].x = 216;
+	boxes[26].y = 140;
+	boxes[26].width = 10;
+	boxes[26].height = 146;
 
-	boxes[27].x = 479;
-	boxes[27].y = 286;
+	boxes[27].x = 255;
+	boxes[27].y = 220;
 	boxes[27].width = 10;
-	boxes[27].height = 19;
+	boxes[27].height = 70;
 
-	boxes[28].x = 458;
-	boxes[28].y = 306;
-	boxes[28].width = 10;
-	boxes[28].height = 60;
+	boxes[28].x = 450;
+	boxes[28].y = 215;
+	boxes[28].width = 7;
+	boxes[28].height = 120;
 
-	boxes[29].x = 508;
-	boxes[29].y = 354;
-	boxes[29].width = 10;
-	boxes[29].height = 55;
+	boxes[29].x = 470;
+	boxes[29].y = 180;
+	boxes[29].width = 7;
+	boxes[29].height = 120;
 
-	Rectangle rect = { 195, 15, 20, 20 };
+	boxes[30].x = 475;
+	boxes[30].y = 220;
+	boxes[30].width = 7;
+	boxes[30].height = 120;
+
+	boxes[31].x = 455;
+	boxes[31].y = 50;
+	boxes[31].width = 10;
+	boxes[31].height = 120;
+
+	boxes[32].x = 460;
+	boxes[32].y = 150;
+	boxes[32].width = 10;
+	boxes[32].height = 30;
+
+	boxes[33].x = 506;
+	boxes[33].y = 0;
+	boxes[33].width = 45;
+	boxes[33].height = 220;
+
+	boxes[34].x = 550;
+	boxes[34].y = 150;
+	boxes[34].width = 38;
+	boxes[34].height = 70;
+
+	boxes[35].x = 520;
+	boxes[35].y = 246;
+	boxes[35].width = 45;
+	boxes[35].height = 66;
+
+	boxes[36].x = 520;
+	boxes[36].y = 246;
+	boxes[36].width = 100;
+	boxes[36].height = 10;
+
+	boxes[37].x = 310;
+	boxes[37].y = 400;
+	boxes[37].width = 10;
+	boxes[37].height = 100;
+
+	Rectangle rect = { 190, 15, 10, 10 };
 	Color rectColor = RED;
 
 	const float moveSpeed = 200.0f;
